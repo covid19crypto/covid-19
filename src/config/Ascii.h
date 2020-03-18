2e1a362134885d9262c5e3b526b715bdb074ff01
COVID19 DEVELOPER <admin@covid19crypto.com>
2020-03-17 23:33:28 -0700

    COVID-19 for Research and Development

---
+++ src/config/Ascii.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The DeroGold Association
+// Copyright (c) 2020, The COVID-19 Developers
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+
+
+" +-+-+-+-+-+-+-+-+\n"
+" |C|O|V|I|D|-|1|9|\n"
+" +-+-+-+-+-+-+-+-+\n";
+
